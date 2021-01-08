3f46bc5c07ca39d83244a3ac06d11299e86e5a59
dymafr <erwan1309@hotmail.com>
2021-01-08 11:33:33 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-  imports: [BrowserModule],
   declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
   providers: []
 })
